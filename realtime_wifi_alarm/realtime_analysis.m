59be43c5bb9e5b2481d6080bc0ccf9b87bc68806
liuminghui233 <549208791@qq.com>
2019-12-16 17:26:29 +0800

    Update realtime_analysis.m

--- realtime_wifi_alarm/realtime_analysis.m
+++ realtime_wifi_alarm/realtime_analysis.m
@@ -1,14 +1,5 @@
-%READ_BF_SOCKET Reads in a file of beamforming feedback logs.
-%   This version uses the *C* version of read_bfee, compiled with
-%   MATLAB's MEX utility.
-%
-% (c) 2008-2011 Daniel Halperin <dhalperi@cs.washington.edu>
-%
-%   Modified by Renjie Zhang, Bingxian Lu.
-%   Email: bingxian.lu@gmail.com
-
 function realtime_analysis()
-load SVMModel.mat;
+load SVMbatchModel.mat;
 while 1
     %% Build a TCP Server and wait for connection
     port = 8090;
@@ -19,19 +10,34 @@ while 1
     fopen(t);
     fprintf('Accept connection from %s\n',t.RemoteHost);
     
+    %% Set plot parameters
+    clf;
+    axis([1,30,-10,30]);
+    t1=0;
+    m1=zeros(30,1);
+    
+    %%  Starting in R2014b, the EraseMode property has been removed from all graphics objects.
+    %%  https://mathworks.com/help/matlab/graphics_transition/how-do-i-replace-the-erasemode-property.html
+    [VER DATESTR] = version();
+    if datenum(DATESTR) > datenum('February 11, 2014')
+        p = plot(t1,m1,'MarkerSize',5);
+    else
+        p = plot(t1,m1,'EraseMode','Xor','MarkerSize',5);
+    end
+    
+    xlabel('Subcarrier index');
+    ylabel('SNR (dB)');
+    
     %% Initialize variables
+    csi_trace = cell(60,1);
+    ct_index = 1;
+    init_flag = 0;
+    yfit = 0;
     csi_entry = [];
     index = -1;                     % The index of the plots which need shadowing
     broken_perm = 0;                % Flag marking whether we've encountered a broken CSI yet
     triangle = [1 3 6];             % What perm should sum to for 1,2,3 antennas
     
-    first_ant_csi = ones(30,1)*nan;
-    second_ant_csi = ones(30,1)*nan;
-    third_ant_csi = ones(30,1)*nan;
-    
-    first_ph_csi = ones(30,1)*nan;
-    second_ph_csi = ones(30,1)*nan;
-    third_ph_csi = ones(30,1)*nan;
     %% Process all entries in socket
     % Need 3 bytes -- 2 byte size field and 1 byte code
     while 1
@@ -54,7 +60,8 @@ while 1
                 fclose(t);
                 return;
             end
-        else if field_len <= t.InputBufferSize  % skip all other info
+        else
+            if field_len <= t.InputBufferSize  % skip all other info
                 fread(t, field_len-1, 'uint8');
                 continue;
             else
@@ -79,56 +86,39 @@ while 1
             end
         end
         
-        index = mod(index+1, 10);
+        index = mod(index+1, 5);
         
         csi = get_scaled_csi(csi_entry);%CSI data
         %You can use the CSI data here.
-        
-        % CSI Amplitude
-        csia=abs(squeeze(csi(1,:,:)).');
-        first_ant_csi=csia(:,1);
-        second_ant_csi=csia(:,2);
-        third_ant_csi=csia(:,3);
-        
-        % CSI Phase
-        csis =  squeeze(csi(1,:,:)).';
-        [csilt, ~] = linear_transform(csis.');
-        csip = angle(csilt.');
-        first_ph_csi=csip(:,1);
-        second_ph_csi=csip(:,2);
-        third_ph_csi=csip(:,3);
+        if init_flag == 0
+            csi_trace{ct_index} = csi;
+            ct_index = ct_index + 1;
+            if ct_index == 60
+                init_flag = 1;
+            end
+        else
+            for i=1:59
+                csi_trace{i} = csi_trace{i+1};
+            end
+            csi_trace{60} = csi;
+        end
+        feature_vetor = get_feature_vetor(csi_trace);
+        yfit = SVMbatchModel.predictFcn(feature_vetor);
         
         %This plot will show graphics about recent 10 csi packets
-        clf;
-        subplot(1,2,1);
-        t1=0;m1=zeros(30,1);
-        p1 = plot(t1,m1,'MarkerSize',5);
-        xlabel('Subcarrier index');
-        ylabel('CSI Amplitude');
-        set(gcf,'units','normalized','position',[0.1 0.1 0.6 0.6]);
-        set(p1(index*3 + 1),'XData', [1:30], 'YData', first_ant_csi, 'color', 'b', 'linestyle', '-');
+        if yfit == 0
+            backColor = [0.1 0.8 0.2 0.1];set(gca, 'color', backColor);
+        else
+            backColor = [0.8 0.2 0.1 0.2];set(gca, 'color', backColor);
+        end
+        set(p(index*7),'XData', [1:30], 'YData', db(abs(squeeze(csi(1,1,:)).')), 'color', 'b', 'linestyle', '-');
         if Nrx > 1
-            set(p1(index*3 + 2),'XData', [1:30], 'YData', second_ant_csi, 'color', 'g', 'linestyle', '-');
+            set(p(index*7 + 1),'XData', [1:30], 'YData', db(abs(squeeze(csi(1,2,:)).')), 'color', 'g', 'linestyle', '-');
         end
         if Nrx > 2
-            set(p1(index*3 + 3),'XData', [1:30], 'YData', third_ant_csi, 'color', 'r', 'linestyle', '-');
+            set(p(index*7 + 2),'XData', [1:30], 'YData', db(abs(squeeze(csi(1,3,:)).')), 'color', 'r', 'linestyle', '-');
         end
-        axis([1,30,0,40]);
-        
-        subplot(1,2,2);
-        t2=0; m2=zeros(30,1);
-        p2 = plot(t2,m2,'MarkerSize',5);
-        xlabel('Subcarrier index');
-        ylabel('CSI Phase');
-        set(p2(index*3 + 1),'XData', [1:30], 'YData',first_ph_csi, 'color', 'b', 'linestyle', '-');
-        if Nrx > 1
-            set(p2(index*3 + 2),'XData', [1:30], 'YData', second_ph_csi, 'color', 'g', 'linestyle', '-');
-        end
-        if Nrx > 2
-            set(p2(index*3 + 3),'XData', [1:30], 'YData', third_ph_csi, 'color', 'r', 'linestyle', '-');
-        end
-        axis([1,30,-0.5,0.5]);
-        
+        axis([1,30,-10,40]);
         drawnow;
         
         csi_entry = [];
