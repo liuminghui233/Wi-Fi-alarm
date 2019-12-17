9f9ec8da5729d8566107b3e9c810f3a27e4448a1
liuminghui233 <549208791@qq.com>
2019-12-17 10:30:26 +0800

    del

--- svm_classifier/get_dataset.m
+++ svm_classifier/get_dataset.m
@@ -1,24 +1,24 @@
 flag = 0;
-for type = 0:7
+for type = 0:1
     if type > 0
         label = 1;
-        labels = ones(60,1);
+        labels = ones(40,1);
     else 
         label = 0;
-        labels = zeros(60,1);
+        labels = zeros(40,1);
     end
-    for index = 1:3
+    for index = 1:6
         filename = ['csi_data/','0',int2str(type),'00',int2str(index),'.dat'] 
-        batch_data = get_batch_data(filename, 0, 60);
+        batch_data = get_batch_data(filename, 0, 200);
         
         if flag == 0
-            dataset90 =[labels (reshape(batch_data,90,60)).'];
+            dataset90 =[labels (reshape(batch_data,450,40)).'];
             flag = 1;
         else
-          dataset90 =  cat(1,dataset90,[labels (reshape(batch_data,90,60)).']);
+          dataset90 =  cat(1,dataset90,[labels (reshape(batch_data,450,40)).']);
         end
     end
 end
 
-save dataset90;
+save dataset;
 
